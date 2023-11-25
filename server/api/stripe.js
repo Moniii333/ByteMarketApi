353c4b15981e555bebd3e002016c4840b01811ff
Monique <Mon1qu3@icloud.com>
2023-11-24 22:25:11 -0700

    updated stripe url

--- server/api/stripe.js
+++ server/api/stripe.js
@@ -30,8 +30,8 @@ router.post('/create-checkout-session/:id', async (req, res) => {
     client_reference_id: user.username,
     line_items: lineItems,
     mode: 'payment',
-    success_url: 'http://localhost:5173/success',
-    cancel_url: 'http://localhost:5173/cancel'
+    success_url: 'https://bytemarket.netlify.app/success',
+    cancel_url: 'https://bytemarket.netlify.app/cancel'
   })
   res.send(JSON.stringify({
     url: session.url
